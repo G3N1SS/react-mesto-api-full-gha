b58a9576a2d10fa585bbca1ff7a9f34311eef0c4
unknown <areg.sarkisyan.99@mail.ru>
2023-12-02 15:03:47 +0300

    final deploy before review

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const { errors } = require('celebrate');
 const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger')
 
-const { PORT = 3000, DB_URL = 'mongodb://localhost:3000/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 
 app.use(cors());
